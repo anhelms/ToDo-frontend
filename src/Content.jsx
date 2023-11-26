ea955edeb047ca715f86143dea4e50dd29b5e4a9
vdacostasanchez <v.dacostasanchez@gmail.com>
2023-11-26 11:47:46 -0500

    adds index action to frontend

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,12 +1,23 @@
 import { ToDosNew } from "./ToDosNew";
+import { ToDosIndex } from "./ToDosIndex";
 import axios from "axios";
 import { useState, useEffect } from "react";
 
 export function Content() {
-  const [toDos, setToDos] = useState();
+  const [toDos, setToDos] = useState([]);
+
+  const handleIndexToDos = () => {
+    console.log("another hello");
+    axios.get("http://localhost:3000/to_dos.json").then((response) => {
+      console.log(response.data, "hello");
+      setToDos(response.data);
+    });
+  };
+
+  useEffect(handleIndexToDos, []);
 
   const handleCreateToDo = (params) => {
-    axios.get("http://localhost:3000/to_dos.json", params).then((response) => {
+    axios.post("http://localhost:3000/to_dos.json", params).then((response) => {
       console.log(response.data);
       setToDos([...toDos, response.data]);
     });
@@ -16,6 +27,7 @@ export function Content() {
     <main>
       <h1>Welcome to React!</h1>
       <ToDosNew onCreateToDo={handleCreateToDo} />
+      <ToDosIndex toDos={toDos} />
     </main>
   );
 }
